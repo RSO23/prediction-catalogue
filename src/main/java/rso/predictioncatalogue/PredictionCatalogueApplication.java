d2eb6a2bff566f7d5bd7dc604ab7745bcf3ddf46
Blarc <jakob.malezic@gmail.com>
2020-12-08 16:35:01 +0100

    init commit

---
+++ src/main/java/rso/predictioncatalogue/PredictionCatalogueApplication.java
@@ -0,0 +1,13 @@
+package rso.predictioncatalogue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PredictionCatalogueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PredictionCatalogueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rso/predictioncatalogue/PredictionCatalogueApplicationTests.java
@@ -0,0 +1,13 @@
+package rso.predictioncatalogue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PredictionCatalogueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
